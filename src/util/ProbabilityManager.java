0dde833bbd2d7f409bc634810129b073347b3bc9
Benardi <jose.nunes@ccc.ufcg.edu.br>
2017-07-10 21:35:17 -0300

    Implementation of module that generates pseudo random numbers

---
+++ src/util/ProbabilityManager.java
@@ -0,0 +1,23 @@
+package util;
+
+import java.util.Random;
+
+public class ProbabilityManager {
+	
+	private Random rand;
+	public ProbabilityManager() {
+		this.rand = new Random();
+		}
+	
+	public int generateValue(int lowerBound, int upperBound){
+		return  rand.ints(lowerBound, upperBound + 1).findFirst().getAsInt();
+	
+	}
+
+	public static void main(String[] args) {
+		ProbabilityManager pm = new ProbabilityManager();
+		for(int i = 0; i < 1000; i++){
+			System.out.println(pm.generateValue(3,7));
+		}
+	}
+}
