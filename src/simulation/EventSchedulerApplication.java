883fb0676e61c8607bcd40e10c293a9976db16ff
Benardi <jose.nunes@ccc.ufcg.edu.br>
2017-07-13 11:05:03 -0300

    Application running now includes loop with iteration on console

--- src/simulation/EventSchedulerApplication.java
+++ src/simulation/EventSchedulerApplication.java
@@ -1,6 +1,7 @@
 package simulation;
 
 import java.sql.Timestamp;
+import java.util.Scanner;
 
 import simulation.event.EventChronologizer;
 import simulation.util.OutputManager;
@@ -8,12 +9,33 @@ import simulation.util.OutputManager;
 public class EventSchedulerApplication {
 
 	public static void main(String[] args) {
-		
-		Timestamp timeStamp = new Timestamp(System.currentTimeMillis());
-		EventChronologizer th = new EventChronologizer();
-		long exectTimeMilli = 2000;
-		th.simulates(exectTimeMilli);
-		OutputManager.getInstance().logData(timeStamp, "SimulationResults.txt");
+
+		Scanner sc = null;
+
+		try {
+			while (true) {
+				Timestamp timeStamp = new Timestamp(System.currentTimeMillis());
+				EventChronologizer th = new EventChronologizer();
+
+				sc = new Scanner(System.in);
+				System.out.println("Please insert time of simulation in milliseconds");
+				long exectTimeMilli = sc.nextLong();
+				th.simulates(exectTimeMilli);
+				OutputManager.getInstance().logData(timeStamp, "SimulationResults.txt");
+				System.out.println("Would you like to run another simulation? Y/N");
+				String answer = sc.next();
+
+				if (answer.toLowerCase().contains("n")) {
+					System.out.println("Application terminated");
+					break;
+				}
+			}
+		} catch (Exception e) {
+
+		} finally {
+			sc.close();
+
+		}
 
 	}
 }
